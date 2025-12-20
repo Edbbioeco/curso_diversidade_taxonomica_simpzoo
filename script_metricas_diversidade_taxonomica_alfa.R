fe25e3f0fcf5f993a509c2dcaf53a1d51737cc02
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-12-20 12:50:09 -0300

    Add files via upload

---
+++ script_metricas_diversidade_taxonomica_alfa.R
@@ -0,0 +1,19 @@
+# Pacotes ----
+
+# Dados ----
+
+# Diagrama de Whittaker ----
+
+# Índices clássicos de diversidade ----
+
+## Riqueza ----
+
+## Shannon-Wiener ----
+
+## Gini-Simpson ----
+
+## Equitabilidade de Pielou ----
+
+# Indices de Hill ----
+
+# Modelos lineares usando diversidade taxonômica alfa ----
