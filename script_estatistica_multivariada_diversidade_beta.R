7c2aa8145b994f215defc8cf1e36b286ffd8b5dc
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-12-20 12:58:03 -0300

    Add files via upload

---
+++ script_estatistica_multivariada_diversidade_beta.R
@@ -0,0 +1,31 @@
+# Pacotes ----
+
+# Dados ----
+
+# Análises de agrupamento ----
+
+## Agrupamento hierárquico ----
+
+## Agrupamento não-hierárquico ----
+
+# Ordenação de uma matriz ----
+
+## PCA ----
+
+## PCoA ----
+
+## NMDS ----
+
+# Ordenação de uma matriz e variáveis associadas ----
+
+## RDA ----
+
+## DB-RDA ----
+
+## RDAP ----
+
+## ANOSIM ----
+
+## Permanova ----
+
+## Regressão de matrizes ----
