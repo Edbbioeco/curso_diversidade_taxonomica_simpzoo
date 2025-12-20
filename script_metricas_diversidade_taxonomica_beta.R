5621e1809bd713f72d64a5ba4549594abb7e3504
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-12-20 12:54:00 -0300

    Add files via upload

---
+++ script_metricas_diversidade_taxonomica_beta.R
@@ -0,0 +1,13 @@
+# Pacotes ----
+
+# Dados ----
+
+# Gráfico de barras de composição de espécies ----
+
+# Diversidade beta baseada em incidência de espécies ----
+
+## Índice de Sorensen ----
+
+## Índice de Jaccard ----
+
+# Diversdade beta baseada em abundância de espécies ----
