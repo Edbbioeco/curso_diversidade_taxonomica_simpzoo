27bec3a61e66fa98059707e45c59578d8534be1a
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-18 10:03:34 -0300

    Script para analisar as métricas de diversidade taxonômica alfa

--- script_metricas_diversidade_taxonomica_alfa.R
+++ script_metricas_diversidade_taxonomica_alfa.R
@@ -1,8 +1,59 @@
 # Pacotes ----
 
+library(tidyverse)
+
+library(vegan)
+
 # Dados ----
 
+## Importando ----
+
+com <- readr::read_csv("composicao_anuros.csv")
+
+## Visualizando ----
+
+com
+
+com |> dplyr::glimpse()
+
+## Adicionar nome às linhas ----
+
+rownames(com) <- c(paste0("Comunidade 0", 1:9),
+                   paste0("Comunidade ", 10:14))
+
+com |> rownames()
+
 # Diagrama de Whittaker ----
+
+## Calculando ----
+
+whitakker <- com |>
+  tibble::rownames_to_column() |>
+  dplyr::rename("Comunidade" = rowname) |>
+  tidyr::pivot_longer(cols = dplyr::where(is.numeric),
+                      names_to = "Especie",
+                      values_to = "Abundancia") |>
+  dplyr::summarise(Abundancia = Abundancia |> sum(),
+                   .by = c(Especie, Comunidade)) |>
+  dplyr::arrange(Abundancia |> dplyr::desc()) |>
+  dplyr::mutate(Rank = dplyr::row_number(),
+                .by = Comunidade) |>
+  dplyr::filter(Abundancia > 0)
+
+whitakker
+
+## Gráfico ----
+
+whitakker |>
+  ggplot(aes(Rank, Abundancia)) +
+  geom_line(linewidth = 1,
+            color = "black") +
+  facet_wrap(~Comunidade, scales = "free") +
+  theme_classic()
+
+ggsave(filename = "diversidade_taxonomica_whittaker.png",
+       height = 10,
+       width = 12)
 
 # Índices clássicos de diversidade ----
 
