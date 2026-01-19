d8ceeeec77aff5180a80ca09ed6b3d7758d04c42
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-19 10:01:42 -0300

    Script para analisar as métricas de diversidade taxonômica beta

--- script_metricas_diversidade_taxonomica_beta.R
+++ script_metricas_diversidade_taxonomica_beta.R
@@ -1,6 +1,40 @@
-# Pacotes ----
+# Instalando pacotes ----
+
+install.packages(c("tidyverse",
+                   "betapart",
+                   "reshape2",
+                   "devtools"))
+
+devtools::install_github("Edbbioeco/ordenaR")
+
+# Carregando pacotes ----
+
+library(tidyverse)
+
+library(ordenaR)
+
+library(betapart)
+
+library(reshape2)
 
 # Dados ----
+
+## Importando ----
+
+com <- readr::read_csv("composicao_anuros.csv")
+
+## Visualizando ----
+
+com
+
+com |> dplyr::glimpse()
+
+## Adicionar nome às linhas ----
+
+rownames(com) <- c(paste0("Comunidade 0", 1:9),
+                   paste0("Comunidade ", 10:14))
+
+com |> rownames()
 
 # Gráfico de barras de composição de espécies ----
 
