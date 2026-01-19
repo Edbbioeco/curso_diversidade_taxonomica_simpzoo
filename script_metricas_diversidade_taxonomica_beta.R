ca804ac7fdf8a7be96d92eda99bad8adb235caf6
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-19 10:09:35 -0300

    Script para analisar as métricas de diversidade taxonômica beta

--- script_metricas_diversidade_taxonomica_beta.R
+++ script_metricas_diversidade_taxonomica_beta.R
@@ -36,7 +36,33 @@ rownames(com) <- c(paste0("Comunidade 0", 1:9),
 
 com |> rownames()
 
-# Gráfico de barras de composição de espécies ----
+# Gráfico de composição de espécies por abundância ----
+
+## Gráfico de barras ----
+
+com |>
+  tibble::rownames_to_column() |>
+  ordenaR::order_bar(gradient = "rowname",
+                     species = 2:18,
+                     direct = FALSE) +
+  theme(axis.text.x = element_text(angle = 90))
+
+ggsave(filename = "diversidade_taxonomica_beta_grafico_barras.png",
+       height = 10,
+       width = 12)
+
+## Gráfico de circulos ----
+
+com |>
+  tibble::rownames_to_column() |>
+  ordenaR::order_circle(gradient = "rowname",
+                        species = 2:18,
+                        direct = FALSE) +
+  theme(axis.text.x = element_text(angle = 90))
+
+ggsave(filename = "diversidade_taxonomica_beta_grafico_circulos.png",
+       height = 10,
+       width = 12)
 
 # Diversidade beta baseada em incidência de espécies ----
 
