5a40cbbdc73bcacf6b403544d1555a9bf9d8edb9
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-21 08:02:10 -0300

    Script para analaisar a distribuição espacial dos valores de diversidade taxonômica

---
+++ script_distribuição_de_diversidade.R
@@ -0,0 +1,16 @@
+# Instalando os pacotes -----
+
+# Carregando os pacotes ----
+
+# Dados ----
+
+# Criando a grade ----
+
+# Montando as comunidades por grades ----
+
+# Distribuição dos valores de riqueza ----
+
+# Distribuição dos valores de diversidade alfa ----
+
+# Distribuição dos valores de diversidade beta ----
+
