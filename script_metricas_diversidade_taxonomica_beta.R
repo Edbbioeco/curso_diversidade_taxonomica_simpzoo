4d0ab86f0664e53a878542c774ddfa321131299d
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-24 10:44:44 -0300

    script para calcular as métricas de diversidade taxonomica beta

--- script_metricas_diversidade_taxonomica_beta.R
+++ script_metricas_diversidade_taxonomica_beta.R
@@ -232,7 +232,7 @@ df_jaccard |>
        y = NULL) +
   scale_fill_viridis_c(guide = guide_colorbar(title.position = "top",
                                               title.hjust = 0.5,
-                                              barwidth = 20,
+                                              barwidth = 25,
                                               frame.colour = "black",
                                               ticks.colour = "black",
                                               ticks.linewidth = 1),
