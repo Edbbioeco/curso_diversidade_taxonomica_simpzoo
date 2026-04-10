e3bc2725f852c734fac3cee3b26b22ceba7d4884
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-10 18:19:59 -0300

    Script para instalar os pacotes necessários

---
+++ instalar_pacotes.R
@@ -0,0 +1,23 @@
+if(require(c("tidyverse",
+             "sf",
+             "vegan",
+             "terra",
+             "tidyterra",
+             "betapart",
+             "scales",
+             "iNEXT",
+             "reshape2",
+             "devtools")) == FALSE) {
+
+  install.packages(c("tidyverse",
+                     "sf",
+                     "vegan",
+                     "terra",
+                     "tidyterra",
+                     "betapart",
+                     "scales",
+                     "iNEXT",
+                     "reshape2",
+                     "devtools"))
+
+}
